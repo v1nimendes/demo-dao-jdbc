b2828807e68576003d89b7c8760ee4ccac9ef0dc
Vinicius <viniciusmendes2802@gmail.com>
2024-05-02 16:00:37 -0300

    update implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -40,6 +40,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller findById =====");
+		seller  = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
